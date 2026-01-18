4822777c64ecc2bb7efb91e50702ebe231b2bb0a
dandeandean <dandean44523@gmail.com>
2026-01-18 11:48:11 -0500

    system updates

--- common/packages.nix
+++ common/packages.nix
@@ -1,30 +1,32 @@
 { pkgs, lib, ... }: {
-  programs.zsh.enable = true;
-  environment.systemPackages = with pkgs;
-    [
-      # System
-      wget
-      kubernetes
-      kubectl
-      kompose
-      python3
-      htop
-      nmap
-      git
+  config = {
+    programs.zsh.enable = true;
+    environment.systemPackages = with pkgs;
+      [
+        # System
+        wget
+        kubernetes
+        kubectl
+        kompose
+        python3
+        htop
+        nmap
+        git
 
-      # Editing
-      vim
-      neovim
-      ripgrep
-      zk
-      luarocks
-      lua
-      rustc
-      cargo
-      go
-      nodejs_22
-      unzip
-      nixfmt-classic
-    ] ++ lib.optionals (builtins.currentSystem == "aarch64-linux") [ gcc ]
-    ++ lib.optionals (builtins.currentSystem == "X86_64-linux") [ gcc_multi ];
+        # Editing
+        vim
+        neovim
+        ripgrep
+        zk
+        luarocks
+        lua
+        rustc
+        cargo
+        go
+        nodejs_22
+        unzip
+        nixfmt-classic
+      ] ++ lib.optionals (builtins.currentSystem == "aarch64-linux") [ gcc ]
+      ++ lib.optionals (builtins.currentSystem == "X86_64-linux") [ gcc_multi ];
+  };
 }
