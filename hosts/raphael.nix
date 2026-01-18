a0cd978f1b66be7d17b0e8b4ac92da6bc8367237
dandeandean <dandean44523@gmail.com>
2026-01-18 17:31:05 -0500

    apple

--- hosts/raphael.nix
+++ hosts/raphael.nix
@@ -10,6 +10,7 @@
     sshBox.enable = true;
     sshBox.doSecurity = false;
     isK3sNode.enable = true;
+    nixpkgs.config.allowUnfree = true;
 
     # Don't change unless you like pain
     system.stateVersion = "25.05"; # Did you read the comment?
