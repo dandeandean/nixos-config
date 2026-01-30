13a009685ba9dc70d4dec541b5a230bd5c57b6db
dandeandean <dandean44523@gmail.com>
2026-01-30 06:43:19 -0500

    add leo as worker

--- common/system.nix
+++ common/system.nix
@@ -8,6 +8,7 @@
     };
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
     system.copySystemConfiguration = true;
+    networking.networkmanager.enable = true;
     programs.zsh.enable = true;
     environment.systemPackages = with pkgs;
       [
--- hosts/leonardo.nix
+++ hosts/leonardo.nix
@@ -6,22 +6,12 @@
     /etc/nixos/apple-silicon-support
   ];
   config = {
+    networking.hostName = "leonardo";
     bloat.enable = false;
-    # Use the systemd-boot EFI boot loader.
+    isK3sNode.enable = true;
+    isK3sNode.isServer = false;
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = false;
-
-    networking.hostName = "leonardo"; # Define your hostname.
-    networking.networkmanager.enable = true;
-    environment.systemPackages = with pkgs; [
-      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-      wget
-      neovim
-      gh
-      tree
-      git
-    ];
-
     system.stateVersion = "25.11"; # Don't change
   };
 }
