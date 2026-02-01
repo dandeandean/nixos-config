2b104194b0dee5c7f60b82d8dd15a4fbf40415a3
dandeandean <dandean44523@gmail.com>
2026-02-01 13:17:16 -0500

    add other config for michealangelo

--- hosts/michelangelo.nix
+++ hosts/michelangelo.nix
@@ -6,6 +6,15 @@
     /etc/nixos/apple-silicon-support
   ];
   config = {
+
+    networking.hostName = "michelangelo";
+
+    # Use the systemd-boot EFI boot loader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = false;
+    networking.networkmanager.enable = true; # nmcli
+    environment.systemPackages = with pkgs; [ vim wget gh git neovim ];
+    system.stateVersion = "25.11"; # Did you read the comment?
     hardware = {
       apple.touchBar = { enable = true; };
       graphics = { enable = true; };
