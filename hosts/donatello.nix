803b74b0c08bfab4acfa7129709567579c8c3159
dandeandean <dandean44523@gmail.com>
2026-04-26 20:33:18 -0400

    refactor donatello

--- hosts/donatello.nix
+++ hosts/donatello.nix
@@ -5,49 +5,19 @@
   ...
 }:
 {
-
-  imports = [ ../users/ddd.nix ];
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
+  imports = [
+    ../users/ddd.nix
+    ../common
   ];
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Don't start sleeping when we close the lid & plugged in
-  services.logind.lidSwitchExternalPower = "ignore";
-
-  networking.hostName = "donatello"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-
-  environment.systemPackages = with pkgs; [
-    neovim
-    vim
-    wget
-    git
-    gh
-  ];
-
-  services.k3s.enable = true;
-  virtualisation = {
-    containers.enable = true;
-    podman = {
-      enable = true;
-      dockerCompat = true;
-      defaultNetwork.settings.dns_enabled = true;
-    };
+  config = {
+    networking.hostName = "donatello"; # Define your hostname.
+    # Don't start sleeping when we close the lid & plugged in
+    services.logind.lidSwitchExternalPower = "ignore";
+    # Use the systemd-boot EFI boot loader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    isK3sNode.enable = true;
+    isK3sNode.isServer = false;
+    system.stateVersion = "25.05"; # Did you read the comment?
   };
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
-
-  system.stateVersion = "25.05"; # Did you read the comment?
-
 }
